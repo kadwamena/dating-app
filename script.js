f7976439ee3907c1c733566ebe7874e27e936bb5
Kwabena Dwamena <kadwamena@gmail.com>
2021-04-26 15:45:36 +0000

    min and max age properties added

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
 var users = [
   {
     name: "Lucy",
+    age: 24,
     gender: "F",
     hobby: "pets",
     avatar: "avatar1.png",
   },
   {
     name: "Betty",
+    age: 20,
     gender: "F",
     hobby: "pets",
     avatar: "avatar2.png",
   },
   {
     name: "Ronald",
+    age: 28,
     gender: "M",
     hobby: "music",
     avatar: "avatar3.png",
   },
   {
     name: "Christopher",
+    age: 26,
     gender: "M",
     hobby: "sports",
     avatar: "avatar4.png",
   },
   {
     name: "Ximena",
+    age: 19,
     gender: "F",
     hobby: "reading",
     avatar: "avatar5.png",
   },
   {
     name: "Paul",
+    age: 22,
     gender: "M",
     hobby: "shopping",
     avatar: "avatar6.png",
   },
   {
     name: "Charlie",
+    age: 25,
     gender: "M",
     hobby: "pets",
     avatar: "avatar7.png",
@@ -56,28 +63,39 @@ window.addEventListener("load", function () {
     var s = genderField.selectedIndex;
     var gender = genderField.options[s].value;
 
+    var minAge = document.getElementById("min-age");
+    var maxAge = document.getElementById("max-age");
+
     var resultsHtml = "";
     var usersLength = users.length;
 
     for (var i = 0; i < usersLength; i++) {
       //check gender
       if (gender == "A" || gender == users[i].gender) {
-        //check hobby
-        if (hobby == "" || hobby == users[i].hobby) {
-          resultsHtml +=
-            '<div class="person-row">\
+        //check age
+        if (
+          (minAge.value == "" || users[i].age >= minAge.value) &&
+          (maxAge.value == "" || users[i].age <= maxAge.value)
+        )
+          if (hobby == "" || hobby == users[i].hobby) {
+            //check hobby
+            resultsHtml +=
+              '<div class="person-row">\
                          <img src="assets/' +
-            users[i].avatar +
-            '" />\
+              users[i].avatar +
+              '" />\
                          <div class="person-info">\
-                         <div>' +
-            users[i].name +
-            "</div>\
-                         <div>" +
-            users[i].hobby +
-            "</div></div>\
+                         <div class="person-name">' +
+              users[i].name +
+              '</div>\
+            <div class="person-age">' +
+              users[i].age +
+              '</div>\
+            <div class="person-hobby">' +
+              users[i].hobby +
+              "</div></div>\
                           <button>Add as friend</button></div>";
-        }
+          }
       }
     }
 
@@ -87,4 +105,4 @@ window.addEventListener("load", function () {
   var searchBtn = document.getElementById("searchBtn");
 
   searchBtn.addEventListener("click", search);
-});       
+});
